6f789c0c4b5c11a6aaf89edb5450f05b2ba9f7ff
Bence Gombkötő <kucukasd@gmail.com>
2024-07-09 17:14:37 +0200

    refactor - add scale option

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const inputNumber = document.getElementById("inputNum");
-const startNumber = document.getElementById("startNum");
-const autoButton = document.getElementById("autoBtn");
-const startButton = document.getElementById("startBtn");
-const stopButton = document.getElementById("stopBtn");
-const inputSpeed = document.getElementById("inputSpeed");
-const setSpeedBtn = document.getElementById("setSpeedBtn");
-
-let autoInterval = null;
-
 // Function to compute the Collatz sequence
 function collatz(n) {
   let sequence = [n];
@@ -22,61 +12,6 @@ function collatz(n) {
   return sequence;
 }
 
-// Set up SVG dimensions
-const width = 720;
-const height = 560;
-const margin = { top: 30, right: 20, bottom: 30, left: 100 };
-
-// Create an SVG container
-const svg = d3.select("svg").attr("width", width).attr("height", height);
-
-// Set up scales
-const x = d3.scaleLinear().range([margin.left, width - margin.right]);
-
-const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
-
-const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-// Append x-axis and y-axis once
-svg
-  .append("g")
-  .attr("class", "x-axis")
-  .attr("transform", `translate(0,${height - margin.bottom})`);
-
-svg
-  .append("g")
-  .attr("class", "y-axis")
-  .attr("transform", `translate(${margin.left},0)`);
-
-// Append a text element for displaying the current starting number
-const text = svg
-  .append("text")
-  .attr("x", width / 2)
-  .attr("y", margin.top)
-  .attr("text-anchor", "middle")
-  .attr("dy", "-8px")
-  .style("font-size", "2rem");
-
-// Append a text element for displaying the hovered number
-const hoverText = svg
-  .append("text")
-  .attr("x", width - margin.right)
-  .attr("y", margin.top)
-  .attr("text-anchor", "end")
-  .attr("dy", "-8px")
-  .style("font-size", "1.5rem");
-
-let firstNumber = 4;
-let allSequences = [];
-let maxX = 0;
-let maxY = 0;
-let speed = 3000;
-
-function resetScale() {
-  maxX = 0;
-  maxY = 0;
-}
-
 function setupGraph() {
   // Initialize empty domains for x and y scales
   x.domain([0, 1]);
@@ -87,27 +22,7 @@ function setupGraph() {
   svg.select(".y-axis").call(d3.axisLeft(y));
 }
 
-setupGraph();
-
-// Function to draw the Collatz sequence with animation
-function drawCollatzSequence(firstNumber) {
-  const sequence = collatz(firstNumber);
-  allSequences.push(sequence);
-
-  // Update the maximum x and y values
-  maxX = Math.max(maxX, sequence.length - 1);
-  maxY = Math.max(maxY, d3.max(sequence));
-
-  // Update scales
-  x.domain([0, maxX]);
-  y.domain([0, maxY]);
-
-  // Create line generator
-  const line = d3
-    .line()
-    .x((d, i) => x(i))
-    .y((d) => y(d));
-
+function applyPath(svg, sequence, line, firstNumber) {
   // Append a group for the sequence
   const g = svg.append("g").attr("class", "sequence");
 
@@ -137,8 +52,30 @@ function drawCollatzSequence(firstNumber) {
     .duration(speed / 2)
     .ease(d3.easeLinear)
     .attr("stroke-dashoffset", 0);
+}
 
-  // Update axes
+function updateScale(sequence, x, y) {
+  if (scaleMode.value === "max") {
+    // Update the maximum x and y values
+    maxX = Math.max(maxX, sequence.length - 1);
+    maxY = Math.max(maxY, d3.max(sequence));
+  } else {
+    // Follow the new sequence values
+    maxX = sequence.length - 1;
+    maxY = d3.max(sequence);
+  }
+
+  // Update scales
+  x.domain([0, maxX]);
+  y.domain([0, maxY]);
+}
+
+function resetScale() {
+  maxX = 0;
+  maxY = 0;
+}
+
+function axisAnimation(svg, x, y, speed) {
   svg
     .select(".x-axis")
     .transition()
@@ -149,6 +86,22 @@ function drawCollatzSequence(firstNumber) {
     .transition()
     .duration(speed / 4)
     .call(d3.axisLeft(y));
+}
+
+// Function to draw the Collatz sequence with animation
+function drawCollatzSequence(firstNumber) {
+  const sequence = collatz(firstNumber);
+  allSequences.push(sequence);
+
+  // Create line generator
+  const line = d3
+    .line()
+    .x((d, i) => x(i))
+    .y((d) => y(d));
+
+  updateScale(sequence, x, y);
+  axisAnimation(svg, x, y, speed);
+  applyPath(svg, sequence, line, firstNumber);
 
   text.text(`Number ${firstNumber}`);
 
@@ -162,6 +115,68 @@ function updateVisualization() {
   firstNumber++;
 }
 
+const inputNumber = document.getElementById("inputNum");
+const startNumber = document.getElementById("startNum");
+const autoButton = document.getElementById("autoBtn");
+const startButton = document.getElementById("startBtn");
+const stopButton = document.getElementById("stopBtn");
+const inputSpeed = document.getElementById("inputSpeed");
+const setSpeedBtn = document.getElementById("setSpeedBtn");
+const scaleMode = document.getElementById("scaleMode");
+
+// Set up SVG dimensions
+const width = 720;
+const height = 560;
+const margin = { top: 30, right: 20, bottom: 30, left: 100 };
+
+// Create an SVG container
+const svg = d3.select("svg").attr("width", width).attr("height", height);
+// Append x-axis and y-axis once
+svg
+  .append("g")
+  .attr("class", "x-axis")
+  .attr("transform", `translate(0,${height - margin.bottom})`);
+
+svg
+  .append("g")
+  .attr("class", "y-axis")
+  .attr("transform", `translate(${margin.left},0)`);
+
+// Set up scales
+const x = d3.scaleLinear().range([margin.left, width - margin.right]);
+const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Append a text element for displaying the current starting number
+const text = svg
+  .append("text")
+  .attr("x", width / 2)
+  .attr("y", margin.top)
+  .attr("text-anchor", "middle")
+  .attr("dy", "-8px")
+  .style("font-size", "2rem");
+
+// Append a text element for displaying the hovered number
+const hoverText = svg
+  .append("text")
+  .attr("x", width - margin.right)
+  .attr("y", margin.top)
+  .attr("text-anchor", "end")
+  .attr("dy", "-8px")
+  .style("font-size", "1.5rem");
+
+let autoInterval = null;
+let firstNumber = 4;
+let allSequences = [];
+let maxX = 0;
+let maxY = 0;
+let speed = 3000;
+
+setupGraph();
+
+//////////////////////////////////////////////////////////////////
+// Event listeners
 autoButton.addEventListener("click", () => {
   if (!isNaN(firstNumber)) {
     firstNumber = parseInt(startNumber.value);
